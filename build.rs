4b48c1e6a873a4873dfaff9b26deb25cdc253e9b
Christian Mazakas <christian.mazakas@gmail.com>
2022-09-05 10:15:45 -0700

    Cleanup build script to use environment variables

--- build.rs
+++ build.rs
@@ -1,18 +1,21 @@
 extern crate cc;
 
 fn main() {
-    let liburing_include = "/home/exbigboss/cpp/installed/include";
-    let liburing = "/home/exbigboss/cpp/installed/lib";
-    println!("cargo:rustc-link-search={liburing}");
-    println!("cargo:rustc-link-lib=static=uring");
+  let include_dir = std::env::var("RIO_LIBURING_INCLUDE_DIR")
+        .expect("RIO_LIBURING_INCLUDE_DIR was not defined by the environment. It must be set to the include dir of the liburing installation.");
 
-    println!("cargo:rerun-if-changed=src/liburing/lib.c");
-    cc::Build::new()
-        .file("src/liburing/lib.c")
-        .include(liburing_include)
-        .compile("rio-iouring");
+  let library_dir = std::env::var("RIO_LIBURING_LIBRARY_DIR").expect("RIO_LIBURING_LIBRARY_DIR was not defined by the environment. It must be set to the library dir of the liburing installation.");
 
-    let out_dir = std::env::var("OUT_DIR").unwrap();
-    println!("cargo:rustc-link-search={out_dir}");
-    println!("cargo:rustc-link-lib=static=rio-iouring");
+  println!("cargo:rustc-link-search={library_dir}");
+  println!("cargo:rustc-link-lib=static=uring");
+
+  println!("cargo:rerun-if-changed=src/liburing/lib.c");
+  cc::Build::new()
+    .file("src/liburing/lib.c")
+    .include(include_dir)
+    .compile("rio-iouring");
+
+  let out_dir = std::env::var("OUT_DIR").unwrap();
+  println!("cargo:rustc-link-search={out_dir}");
+  println!("cargo:rustc-link-lib=static=rio-iouring");
 }
