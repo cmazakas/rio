494ce4b01c905eea4257f41cd5a5ba192792ea8b
Christian Mazakas <christian.mazakas@gmail.com>
2022-10-27 21:19:25 -0700

    Add tests for users cross-posting executors across different IoContexts

---
+++ tests/post_leak_tests.rs
@@ -0,0 +1,52 @@
+#[test]
+#[ignore]
+#[should_panic]
+fn cross_io_contexts() {
+  /***
+   * This test thankfully panics because the task_ctx for ex2 isn't set when ioc1.run() begins.
+   *
+   * In in the ideal case, we keep it this way so as to protect the user from themselves.
+   *
+   * TODO: find out why we leak 2 FDs from `io_uring_queue_init` here
+   */
+  let mut ioc1 = rio::IoContext::new();
+  let mut ioc2 = rio::IoContext::new();
+
+  let ex1 = ioc1.get_executor();
+  let ex2 = ioc2.get_executor();
+
+  ioc1.post({
+    Box::pin(async move {
+      let mut timer = rio::io::Timer::new(ex2);
+      timer.expires_after(std::time::Duration::from_millis(250));
+      timer.async_wait().await.unwrap();
+    })
+  });
+
+  ioc2.post(Box::pin(async move {
+    let mut timer = rio::io::Timer::new(ex1);
+    timer.expires_after(std::time::Duration::from_millis(250));
+    timer.async_wait().await.unwrap();
+  }));
+
+  ioc1.post({
+    let ex = ioc1.get_executor();
+    Box::pin(async move {
+      let mut timer = rio::io::Timer::new(ex);
+      timer.expires_after(std::time::Duration::from_millis(250));
+      timer.async_wait().await.unwrap();
+    })
+  });
+
+  ioc2.post({
+    let ex = ioc2.get_executor();
+    Box::pin(async move {
+      let mut timer = rio::io::Timer::new(ex);
+      timer.expires_after(std::time::Duration::from_millis(250));
+      timer.async_wait().await.unwrap();
+    })
+  });
+
+  ioc1.run();
+  ioc2.run();
+}
