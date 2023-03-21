246d47a84a3edca31ace366afc93c50637040be3
Christian Mazakas <christian.mazakas@gmail.com>
2023-03-20 20:27:46 -0700

    Rearrange tcp module

--- src/ip/tcp.rs
+++ src/ip/tcp.rs
@@ -51,30 +51,176 @@ pub struct WriteFuture<'a> {
   _m: std::marker::PhantomData<&'a mut Socket>,
 }
 
-fn make_kernel_timespec(
-  t: std::time::Duration,
-) -> fiona::libc::kernel_timespec {
-  let (tv_sec, tv_nsec) = (
-    i64::try_from(t.as_secs()).unwrap(),
-    i64::try_from(t.subsec_nanos()).unwrap(),
-  );
-  fiona::libc::kernel_timespec { tv_sec, tv_nsec }
+impl Acceptor {
+  #[must_use]
+  pub fn new(ex: &fiona::Executor) -> Self {
+    Self {
+      fd: -1,
+      ex: ex.clone(),
+    }
+  }
+
+  pub fn listen(
+    &mut self,
+    ipv4_addr: u32,
+    port: u16,
+  ) -> Result<(), fiona::Errno> {
+    match fiona::liburing::make_ipv4_tcp_server_socket(ipv4_addr, port) {
+      Ok(fd) => {
+        self.fd = fd;
+        Ok(())
+      }
+      Err(e) => Err(unsafe { std::mem::transmute(e) }),
+    }
+  }
+
+  pub fn async_accept(&mut self) -> AcceptFuture {
+    assert!(self.fd > 0);
+    let addr_in = libc::sockaddr_in {
+      sin_family: 0,
+      sin_addr: libc::in_addr { s_addr: 0 },
+      sin_port: 0,
+      sin_zero: Default::default(),
+    };
+
+    let fds = fiona::op::FdState::new(
+      self.fd,
+      fiona::op::Op::Accept(fiona::op::AcceptState {
+        addr_in,
+        addr_len: std::mem::size_of::<libc::sockaddr_in>() as u32,
+      }),
+    );
+
+    AcceptFuture {
+      ex: self.ex.clone(),
+      fds,
+      _m: std::marker::PhantomData,
+    }
+  }
 }
 
-unsafe fn drop_cancel(
-  p: *mut fiona::op::FdStateImpl,
-  ioc: *mut fiona::IoContextState,
-) {
-  debug_assert!(!p.is_null());
-  debug_assert!(!ioc.is_null());
+impl Socket {
+  fn fd(&self) -> i32 {
+    self.fd
+  }
 
-  if (*p).initiated && !(*p).done {
-    let ring = (*ioc).ring;
-    unsafe {
-      let sqe = fiona::liburing::io_uring_get_sqe(ring);
-      fiona::liburing::io_uring_prep_cancel(sqe, p.cast::<libc::c_void>(), 0);
-      fiona::liburing::io_uring_submit(ring);
+  fn ex(&self) -> fiona::Executor {
+    self.ex.clone()
+  }
+
+  #[must_use]
+  pub fn new(ex: &fiona::Executor) -> Self {
+    Self {
+      fd: fiona::liburing::make_ipv4_tcp_socket().unwrap(),
+      ex: ex.clone(),
+      timeout: std::time::Duration::from_secs(30),
     }
+  }
+
+  #[must_use]
+  pub unsafe fn from_raw(ex: fiona::Executor, fd: i32) -> Self {
+    Self {
+      fd,
+      ex,
+      timeout: std::time::Duration::from_secs(30),
+    }
+  }
+
+  pub fn async_read(&mut self, buf: Vec<u8>) -> ReadFuture {
+    let read_fds = fiona::op::FdState::new(
+      self.fd,
+      fiona::op::Op::Read(fiona::op::ReadState {
+        buf: Some(buf),
+        timer_fds: None,
+      }),
+    );
+
+    ReadFuture {
+      ex: self.ex.clone(),
+      read_fds,
+      timeout: self.timeout,
+      _m: std::marker::PhantomData,
+    }
+  }
+
+  pub fn async_write(&mut self, buf: Vec<u8>) -> WriteFuture {
+    let write_fds = fiona::op::FdState::new(
+      self.fd,
+      fiona::op::Op::Write(fiona::op::WriteState { buf: Some(buf) }),
+    );
+
+    WriteFuture {
+      ex: self.ex.clone(),
+      write_fds,
+      timeout: self.timeout,
+      _m: std::marker::PhantomData,
+    }
+  }
+}
+
+impl Client {
+  #[must_use]
+  pub fn new(ex: &fiona::Executor) -> Self {
+    Self { s: Socket::new(ex) }
+  }
+
+  pub fn async_connect(&mut self, ipv4_addr: u32, port: u16) -> ConnectFuture {
+    let addr_in = libc::sockaddr_in {
+      sin_family: libc::AF_INET as u16,
+      sin_port: port.to_be(),
+      sin_addr: libc::in_addr {
+        s_addr: ipv4_addr.to_be(),
+      },
+      sin_zero: Default::default(),
+    };
+
+    let connect_fds = fiona::op::FdState::new(
+      self.fd(),
+      fiona::op::Op::Connect(fiona::op::ConnectState {
+        addr_in,
+        timer_fds: None,
+      }),
+    );
+
+    ConnectFuture {
+      ex: self.ex(),
+      connect_fds,
+      timeout: self.timeout,
+      _m: std::marker::PhantomData,
+    }
+  }
+}
+
+impl Server {
+  #[must_use]
+  pub fn new(ex: &fiona::Executor) -> Self {
+    Self { s: Socket::new(ex) }
+  }
+}
+
+impl std::ops::Deref for Client {
+  type Target = Socket;
+  fn deref(&self) -> &Self::Target {
+    &self.s
+  }
+}
+
+impl std::ops::DerefMut for Client {
+  fn deref_mut(&mut self) -> &mut Self::Target {
+    &mut self.s
+  }
+}
+
+impl std::ops::Deref for Server {
+  type Target = Socket;
+  fn deref(&self) -> &Self::Target {
+    &self.s
+  }
+}
+
+impl std::ops::DerefMut for Server {
+  fn deref_mut(&mut self) -> &mut Self::Target {
+    &mut self.s
   }
 }
 
@@ -394,54 +540,6 @@ impl<'a> std::future::Future for WriteFuture<'a> {
   }
 }
 
-impl Acceptor {
-  #[must_use]
-  pub fn new(ex: &fiona::Executor) -> Self {
-    Self {
-      fd: -1,
-      ex: ex.clone(),
-    }
-  }
-
-  pub fn listen(
-    &mut self,
-    ipv4_addr: u32,
-    port: u16,
-  ) -> Result<(), fiona::Errno> {
-    match fiona::liburing::make_ipv4_tcp_server_socket(ipv4_addr, port) {
-      Ok(fd) => {
-        self.fd = fd;
-        Ok(())
-      }
-      Err(e) => Err(unsafe { std::mem::transmute(e) }),
-    }
-  }
-
-  pub fn async_accept(&mut self) -> AcceptFuture {
-    assert!(self.fd > 0);
-    let addr_in = libc::sockaddr_in {
-      sin_family: 0,
-      sin_addr: libc::in_addr { s_addr: 0 },
-      sin_port: 0,
-      sin_zero: Default::default(),
-    };
-
-    let fds = fiona::op::FdState::new(
-      self.fd,
-      fiona::op::Op::Accept(fiona::op::AcceptState {
-        addr_in,
-        addr_len: std::mem::size_of::<libc::sockaddr_in>() as u32,
-      }),
-    );
-
-    AcceptFuture {
-      ex: self.ex.clone(),
-      fds,
-      _m: std::marker::PhantomData,
-    }
-  }
-}
-
 impl Drop for Acceptor {
   fn drop(&mut self) {
     if self.fd >= 0 {
@@ -458,131 +556,6 @@ impl Drop for Socket {
   }
 }
 
-impl Socket {
-  fn fd(&self) -> i32 {
-    self.fd
-  }
-
-  fn ex(&self) -> fiona::Executor {
-    self.ex.clone()
-  }
-
-  #[must_use]
-  pub fn new(ex: &fiona::Executor) -> Self {
-    Self {
-      fd: fiona::liburing::make_ipv4_tcp_socket().unwrap(),
-      ex: ex.clone(),
-      timeout: std::time::Duration::from_secs(30),
-    }
-  }
-
-  #[must_use]
-  pub unsafe fn from_raw(ex: fiona::Executor, fd: i32) -> Self {
-    Self {
-      fd,
-      ex,
-      timeout: std::time::Duration::from_secs(30),
-    }
-  }
-
-  pub fn async_read(&mut self, buf: Vec<u8>) -> ReadFuture {
-    let read_fds = fiona::op::FdState::new(
-      self.fd,
-      fiona::op::Op::Read(fiona::op::ReadState {
-        buf: Some(buf),
-        timer_fds: None,
-      }),
-    );
-
-    ReadFuture {
-      ex: self.ex.clone(),
-      read_fds,
-      timeout: self.timeout,
-      _m: std::marker::PhantomData,
-    }
-  }
-
-  pub fn async_write(&mut self, buf: Vec<u8>) -> WriteFuture {
-    let write_fds = fiona::op::FdState::new(
-      self.fd,
-      fiona::op::Op::Write(fiona::op::WriteState { buf: Some(buf) }),
-    );
-
-    WriteFuture {
-      ex: self.ex.clone(),
-      write_fds,
-      timeout: self.timeout,
-      _m: std::marker::PhantomData,
-    }
-  }
-}
-
-impl Client {
-  #[must_use]
-  pub fn new(ex: &fiona::Executor) -> Self {
-    Self { s: Socket::new(ex) }
-  }
-
-  pub fn async_connect(&mut self, ipv4_addr: u32, port: u16) -> ConnectFuture {
-    let addr_in = libc::sockaddr_in {
-      sin_family: libc::AF_INET as u16,
-      sin_port: port.to_be(),
-      sin_addr: libc::in_addr {
-        s_addr: ipv4_addr.to_be(),
-      },
-      sin_zero: Default::default(),
-    };
-
-    let connect_fds = fiona::op::FdState::new(
-      self.fd(),
-      fiona::op::Op::Connect(fiona::op::ConnectState {
-        addr_in,
-        timer_fds: None,
-      }),
-    );
-
-    ConnectFuture {
-      ex: self.ex(),
-      connect_fds,
-      timeout: self.timeout,
-      _m: std::marker::PhantomData,
-    }
-  }
-}
-
-impl Server {
-  #[must_use]
-  pub fn new(ex: &fiona::Executor) -> Self {
-    Self { s: Socket::new(ex) }
-  }
-}
-
-impl std::ops::Deref for Client {
-  type Target = Socket;
-  fn deref(&self) -> &Self::Target {
-    &self.s
-  }
-}
-
-impl std::ops::DerefMut for Client {
-  fn deref_mut(&mut self) -> &mut Self::Target {
-    &mut self.s
-  }
-}
-
-impl std::ops::Deref for Server {
-  type Target = Socket;
-  fn deref(&self) -> &Self::Target {
-    &self.s
-  }
-}
-
-impl std::ops::DerefMut for Server {
-  fn deref_mut(&mut self) -> &mut Self::Target {
-    &mut self.s
-  }
-}
-
 impl<'a> AcceptFuture<'a> {
   #[must_use]
   pub fn get_cancel_handle(&self) -> fiona::op::CancelHandle {
@@ -594,5 +567,32 @@ impl<'a> ConnectFuture<'a> {
   #[must_use]
   pub fn get_cancel_handle(&self) -> fiona::op::CancelHandle {
     fiona::op::CancelHandle::new(self.connect_fds.clone(), self.ex.clone())
+  }
+}
+
+fn make_kernel_timespec(
+  t: std::time::Duration,
+) -> fiona::libc::kernel_timespec {
+  let (tv_sec, tv_nsec) = (
+    i64::try_from(t.as_secs()).unwrap(),
+    i64::try_from(t.subsec_nanos()).unwrap(),
+  );
+  fiona::libc::kernel_timespec { tv_sec, tv_nsec }
+}
+
+unsafe fn drop_cancel(
+  p: *mut fiona::op::FdStateImpl,
+  ioc: *mut fiona::IoContextState,
+) {
+  debug_assert!(!p.is_null());
+  debug_assert!(!ioc.is_null());
+
+  if (*p).initiated && !(*p).done {
+    let ring = (*ioc).ring;
+    unsafe {
+      let sqe = fiona::liburing::io_uring_get_sqe(ring);
+      fiona::liburing::io_uring_prep_cancel(sqe, p.cast::<libc::c_void>(), 0);
+      fiona::liburing::io_uring_submit(ring);
+    }
   }
 }
